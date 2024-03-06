785d2997fb426ea924666a4bb0c20f4a03b7ed16
Lyrismet <ana0203tir@gmail.com>
2024-03-06 20:31:33 +0300

    feat: added vk ui kit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import {
+    AdaptivityProvider,
+    ConfigProvider,
+    AppRoot,
+    SplitLayout,
+    SplitCol,
+    View,
+    Panel,
+    PanelHeader,
+    Header,
+    Group,
+    SimpleCell,
+    usePlatform,
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// eslint-disable-next-line react-refresh/only-export-components
+const AppShell = () => {
+    const platform = usePlatform();
+
+    return (
+        <AppRoot>
+            <SplitLayout header={platform !== 'vkcom' && <PanelHeader delimiter="none" />}>
+                <SplitCol autoSpaced>
+                    <View activePanel="main">
+                        <Panel id="main">
+                            <PanelHeader>VKUI</PanelHeader>
+                            <Group header={<Header mode="secondary">Items</Header>}>
+                                <SimpleCell>Hello</SimpleCell>
+                                <SimpleCell>World</SimpleCell>
+                            </Group>
+                        </Panel>
+                    </View>
+                </SplitCol>
+            </SplitLayout>
+        </AppRoot>
+    );
+};
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+    <ConfigProvider>
+        <AdaptivityProvider>
+            <AppShell />
+        </AdaptivityProvider>
+    </ConfigProvider>,
+);
