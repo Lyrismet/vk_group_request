630875b5c9105a3b7f422c6ef26edf8dd6b8d070
Lyrismet <ana0203tir@gmail.com>
2024-03-07 13:12:38 +0300

    ref: appshell renamed to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,49 +1,18 @@
 import { createRoot } from 'react-dom/client';
 import {
     AdaptivityProvider,
-    ConfigProvider,
-    AppRoot,
-    SplitLayout,
-    SplitCol,
-    View,
-    Panel,
-    PanelHeader,
-    Header,
-    Group,
-    SimpleCell,
-    usePlatform,
+    ConfigProvider
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
+import App from './App';
 
-// eslint-disable-next-line react-refresh/only-export-components
-const AppShell = () => {
-    const platform = usePlatform();
-
-    return (
-        <AppRoot>
-            <SplitLayout header={platform !== 'vkcom' && <PanelHeader delimiter="none" />}>
-                <SplitCol autoSpaced>
-                    <View activePanel="main">
-                        <Panel id="main">
-                            <PanelHeader>VKUI</PanelHeader>
-                            <Group header={<Header mode="secondary">Items</Header>}>
-                                <SimpleCell>Hello</SimpleCell>
-                                <SimpleCell>World</SimpleCell>
-                            </Group>
-                        </Panel>
-                    </View>
-                </SplitCol>
-            </SplitLayout>
-        </AppRoot>
-    );
-};
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
     <ConfigProvider>
         <AdaptivityProvider>
-            <AppShell />
+            <App />
         </AdaptivityProvider>
     </ConfigProvider>,
 );
